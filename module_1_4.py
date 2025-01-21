8ceb6e8859c1f763b4ec451cb353aa864caabe98
Daniel <danny.koshik@gmail.com>
2025-01-21 21:39:26 +0300

    Организация программ и методы строк
    Практическое задание по уроку "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+horror_movie = input('Введите Ваш любимый фильм ужасов: ')
+#print(horror_movie.count(''))
+print(len(horror_movie))
+print(horror_movie.upper())
+print(horror_movie.lower())
+print(horror_movie.replace(' ', ''))
+print(horror_movie[0])
+print(horror_movie[-1])
+
+#Техасская резня бензапилой
