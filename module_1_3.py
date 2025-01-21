d689e4b816554f4bfbe865061bc2cfa7e09280c9
Daniel <danny.koshik@gmail.com>
2025-01-21 21:38:34 +0300

    Динамическая типизация
    Практическое задание по уроку "Динамическая типизация"

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Daniel"
+age = 32
+new_age = age + 10
+is_student = age != new_age
+
+date = 'Name: ' + name, 'Age: ' + str(age), 'New age: ' + str(new_age), 'Is Student: ' + str(is_student)
+print('\n'.join(date))
+
