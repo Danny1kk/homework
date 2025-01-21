3cd5eb49a02d1c358ab64b9f26355727b861082a
Daniel <danny.koshik@gmail.com>
2025-01-21 21:37:46 +0300

    Переменные
    Практическое задание по уроку "Переменные"

---
+++ module_1_2.py
@@ -0,0 +1,13 @@
+    # Задание
+number_of_homework_completed = 12
+number_of_hours = 1.5
+course_title = 'Python'
+time_per_task = number_of_hours / number_of_homework_completed
+print('Курс:', course_title ,', всего задач:', number_of_homework_completed, ', затрачего часов:', number_of_hours, ', среднее время выполнения:', time_per_task)
+
+
+    # Свой пример
+men = 'мне'
+country = 'французские'
+food = 'булочки'
+print(men[0].upper() + men[1:-1] + men[-1], 'нравятся эти', country, food)
