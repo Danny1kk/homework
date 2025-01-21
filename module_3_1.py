4efb5ec96fb6e41722995d5e2ca4c3b9c3796057
Daniel <danny.koshik@gmail.com>
2025-01-21 21:51:15 +0300

    Пространство имён
    Практическое задание по уроку "Пространство имён"

---
+++ module_3_1.py
@@ -0,0 +1,21 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains(string, list_1):
+    count_calls()
+    string = string.lower()
+    list_1 = [item.lower() for item in list_1]
+    return string in list_1
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
