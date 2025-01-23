9d251a9451c727a7dc2d26170e979521a6c2b73e
Daniel <danny.koshik@gmail.com>
2025-01-23 19:43:26 +0300

    ДПЗ "Раз, два, три, четыре, пять... Это не всё?"
    Дополнительное практическое задание по модулю: "Подробнее о функциях"

---
+++ module_3_hard.py
@@ -0,0 +1,35 @@
+def calculate_structure_sum(*args):
+  total_sum = 0
+
+  def recursive_progress(element):
+    nonlocal total_sum
+
+    if isinstance(element, int):
+      total_sum += element
+    elif isinstance(element, str):
+      total_sum += len(element)
+    elif isinstance(element, (list, tuple, set)):
+      for item in element:
+        recursive_progress(item)
+    elif isinstance(element, dict):
+      for key, value in element.items():
+        recursive_progress(key)
+        recursive_progress(value)
+
+  for arg in args:
+    recursive_progress(arg)
+
+  return total_sum
+
+if __name__ == '__main__':
+  data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+  ]
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
